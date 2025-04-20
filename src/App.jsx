9baa4e8ac411bebb8bce888d4361d4f5773e1144
Chaitanya Pansare <chaitanyapansare33607@gmail.com>
2025-04-20 17:21:23 +0530

    Fixing vercel issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   Navigate,
